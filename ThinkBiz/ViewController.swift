4ec1548044cd37c2f44e9d36d4b656e405c8dbc9
Han Yang Chin <han.yang.chin@imersia.com>
2017-03-29 14:35:36 +1300

    Initial Commit

---
+++ ThinkBiz/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ThinkBiz
+//
+//  Created by Han Yang Chin on 29/03/17.
+//  Copyright © 2017 Han Yang Chin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
