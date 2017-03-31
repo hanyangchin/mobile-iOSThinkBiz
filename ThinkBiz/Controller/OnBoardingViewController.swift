f66cd0ed22c878342df81b7be9419fd5b2bc678e
Han Yang Chin <han.yang.chin@imersia.com>
2017-04-01 11:45:30 +1300

    Added basic UI functionality without StackView and constraints

--- ThinkBiz/Controller/OnBoardingViewController.swift
+++ ThinkBiz/Controller/OnBoardingViewController.swift
@@ -10,67 +10,86 @@ import UIKit
 
 class OnBoardingViewController: UIViewController, UIScrollViewDelegate {
     @IBOutlet weak var scrollView: UIScrollView!
-    @IBOutlet weak var titleTextView: UITextView!
     @IBOutlet weak var pageControl: UIPageControl!
+    
+    var onBoardingScreens = [UIView]()
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
         
-        self.scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        let scrollViewWidth: CGFloat = self.scrollView.frame.width
-        let scrollViewHeight: CGFloat = self.scrollView.frame.height
-        
-        self.titleTextView.textAlignment = .center
-        titleTextView.text = "hahahahah let set this first"
-        
-        let imageOne = UIImageView(frame: CGRect(x: 0, y: 0, width: scrollViewWidth, height: scrollViewHeight))
-        imageOne.image = UIImage(named: "lightbulb")
-        imageOne.contentMode = .scaleAspectFit
-        
-        let imageTwo = UIImageView(frame: CGRect(x: scrollViewWidth, y: 0, width: scrollViewWidth, height: scrollViewHeight))
-        imageTwo.image = UIImage(named: "lightbulb")
-        imageTwo.contentMode = .scaleAspectFit
-        
-        self.scrollView.addSubview(imageOne)
-        self.scrollView.addSubview(imageTwo)
-        
-        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width * 2, height: self.scrollView.frame.height)
-        
+//        self.scrollView.frame = CGRect(x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
         self.scrollView.delegate = self
+
+        self.loadOnBoardingScreens()
+
         self.pageControl.currentPage = 0
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    private func loadOnBoardingScreens() -> Void {
+        
+        let onBoardingScreen1 = generateView(withText: "This is supposed to be text 1", imageName: "lightbulb")
+        let onBoardingScreen2 = generateView(withText: "This is supposed to be text 2", imageName: "lightbulb")
+        let onBoardingScreen3 = generateView(withText: "This is supposed to be text 3", imageName: "lightbulb")
+        
+        self.addOnBoardingScreen(screen: onBoardingScreen1)
+        self.addOnBoardingScreen(screen: onBoardingScreen2)
+        self.addOnBoardingScreen(screen: onBoardingScreen3)
     }
     
-    @IBAction func onGetStartedButtonPressed(_ sender: UIButton) {
+    private func generateView(withText text: String!, imageName: String!) -> UIView {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.alignment = .fill
+        stackView.distribution = .fillProportionally
+        stackView.spacing = 30
+        
+        if text.characters.count > 0 {
+            let titleLabel = UILabel()
+            titleLabel.translatesAutoresizingMaskIntoConstraints = false
+            titleLabel.numberOfLines = 3
+            titleLabel.font = UIFont.preferredFont(forTextStyle: .headline)
+            titleLabel.textColor = .black
+            titleLabel.textAlignment = .center
+            titleLabel.text = text
+            stackView.addArrangedSubview(titleLabel)
+        }
+        
+        if imageName.characters.count > 0 {
+            let imageView = UIImageView()
+            imageView.translatesAutoresizingMaskIntoConstraints = false
+            imageView.image = UIImage(named: imageName)
+            imageView.contentMode = .scaleAspectFit
+            stackView.addArrangedSubview(imageView)
+        }
+        
+        return stackView
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
     
     func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
         let pageWidth: CGFloat = scrollView.frame.width
         let currentPage: CGFloat = floor((scrollView.contentOffset.x - pageWidth / 2) / pageWidth) + 1
         
         self.pageControl.currentPage = Int(currentPage)
+    }
+    
+    func addOnBoardingScreen(screen: UIView!) -> Void {
+        let scrollViewWidth: CGFloat = self.scrollView.frame.width
+        let scrollViewHeight: CGFloat = self.scrollView.frame.height
         
-        if Int(currentPage) == 0 {
-            titleTextView.text = "Brainstorm and plan your business ideas in one place"
-        } else if Int(currentPage) == 1 {
-            titleTextView.text = "Second page dsfasdfsafdsafds sdfasdfsaf"
-        }
+        print("\n*******************************\(scrollView.frame.width) \(self.view.frame.width)")
+        
+        self.onBoardingScreens.append(screen)
+        
+        // Modify screen's frame to include offsets
+        screen.frame = CGRect(x: scrollViewWidth * CGFloat(self.onBoardingScreens.count - 1), y: 0, width: scrollViewWidth, height: scrollViewHeight)
+        
+        self.scrollView.contentSize = CGSize(width: self.scrollView.frame.width * CGFloat(onBoardingScreens.count), height: self.scrollView.bounds.height)
+        
+        self.pageControl.numberOfPages = self.onBoardingScreens.count
+        
+        self.scrollView.addSubview(screen)
     }
 
 }
